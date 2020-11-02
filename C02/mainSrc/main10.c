96d6c5ba5dc422a41ac6aabd5bf4b4dc3d4ddf56
Miguel Gueifao <mgueifao@student.42lisboa.com>
2020-11-02 17:43:47 +0000

    String cleaning added

--- C02/mainSrc/main10.c
+++ C02/mainSrc/main10.c
@@ -16,16 +16,19 @@ int main()
 	
 	size = 10;
 	src = "abcdefghijklmnopqrstuvwxyz";
+	strcpy(dst, "");
 	ret = ft_strlcpy(dst, src, size);
 	printf("\nsize: %d\nsrc: %s\ndst: %s\nret: %u", size, src, dst, ret);
 
 	size = 20;
 	src = "123456789";
+	strcpy(dst, "");
 	ret = ft_strlcpy(dst, src, size);
 	printf("\nsize: %d\nsrc: %s\ndst: %s\nret: %u", size, src, dst, ret);
 
 	size = 0;
 	src = "ABCD EFGH IJKL 123 456 789. Mnop";
+	strcpy(dst, "");
 	ret = ft_strlcpy(dst, src, size);
 	printf("\nsize: %d\nsrc: %s\ndst: %s\nret: %u\n", size, src, dst, ret);
 
