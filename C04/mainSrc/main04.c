abf7377f08f1ebdd236bae8d953de97143b3a85e
Miguel Gueifao <mgueifao@student.42lisboa.com>
2020-11-04 15:27:28 +0000

    Flush stdout in order to correct output error.

--- C04/mainSrc/main04.c
+++ C04/mainSrc/main04.c
@@ -8,34 +8,42 @@ int main()
 
 	strcpy(base, "0123456789");
 	printf("\nExpected 123456789, got: ");
+	fflush(stdout);
 	ft_putnbr_base(123456789, base);
 
 	strcpy(base, "01");
 	printf("\nExpected 101010101010, got: ");
+	fflush(stdout);
 	ft_putnbr_base(2730, base);
 
 	strcpy(base, "0123456789abcdef");
 	printf("\nExpected fafabcd159, got: ");
+	fflush(stdout);
 	ft_putnbr_base(264032601, base);
 
 	strcpy(base, "0123456789abcdefghijklmnopqrstuvwxyz");
 	printf("\nExpected \'ola\', got: ");
+	fflush(stdout);
 	ft_putnbr_base(31870, base);
 
 	strcpy(base, "2Liobas4");
 	printf("\nExpected 42Lisboa, got: ");
+	fflush(stdout);
 	ft_putnbr_base(814, base);
 
 	strcpy(base, "");
 	printf("\nExpected nothing (empty base), got: ");
+	fflush(stdout);
 	ft_putnbr_base(814, base);
 
 	strcpy(base, "([{}])([{}])");
 	printf("\nExpected nothing (duplicate base), got: ");
+	fflush(stdout);
 	ft_putnbr_base(814, base);
 
 	strcpy(base, "12-34");
 	printf("\nExpected nothing (sign base), got: ");
+	fflush(stdout);
 	ft_putnbr_base(814, base);
 
 	free(base);
