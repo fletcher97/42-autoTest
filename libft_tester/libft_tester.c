2921f4f96908f84849ee8e71aa4d683e351c64fd
phillcp <filipecabralpinto@gmail.com>
2020-12-09 05:12:40 +0000

    Finished 8 functions in str_tester and their associated tests calls in libft_tests.

--- libft_tester/libft_tester.c
+++ libft_tester/libft_tester.c
@@ -2,6 +2,7 @@
 #include "to_tester.h"
 #include "str_tester.h"
 #include "test.h"
+#include "libft.h"
 
 void test_is()
 {
@@ -48,20 +49,21 @@ void test_to()
 	}
 }
 
-	int results[] = {1, 1, 5, 0, 20, 144};
-	test_fn_t tests[] = {{test_strlen, "strlen", NULL}, {test_strlcpy, "strlcpy", NULL}, {test_strlcat, "strlcat", NULL}, {test_strchr, "strchr", NULL}, {test_strrchr, "strrchr", NULL}, {test_strnstr, "strnstr", NULL}, {test_strncmp, "strncmp", NULL}, {test_strjoin, "strjoin", NULL}, {test_strtrim, "strtrim", NULL}};
-	foreach(test_fn_t *t, tests){
-
 void test_str()
 {
 	/*strlen*/
+	printf("%sINFO: testing strlen.%s\n", GREEN, CLEAR);
 	char *strlen_tests[145] = {"a", "\r", "hello", "", "hello this is a test", "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest"};
 	int errors = 0;
-	printf("%sINFO: testing strlen.%s\n", GREEN, CLEAR);
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < 8; i++)
 	{
-		data_t *temp = malloc(sizeof(data_t));
-		temp->tests = &i;
+		data_t *temp;
+		if(!(temp = malloc(sizeof(data_t)))){
+			printf("%s%sERROR: Failed to allocate memory to test strlen.%s\n", BLACK, RED_BG, CLEAR);
+				errors++;
+				continue;
+		}
+		temp->tests = strlen_tests[i];
 		errors += test(test_strlen, temp);
 		free(temp);
 	}
@@ -70,21 +72,314 @@ void test_str()
 	else
 		printf("%sINFO: a total of %d errors were detected in strlen.%s\n", GREEN, errors, CLEAR);
 	/*strlcpy*/
-	/*strlcat*/
-	/*strchr*/
-	/*strrchr*/
-	/*strnstr*/
-	/*strncmp*/
-	char *strncmp_tests1[21] = {"a", "\r", "hello", "", "hello this is a test", "tests", "abc"};
-	char *strncmp_tests2[21] = {"b", "\r", "hella", "", "hello thi\n is a", "tests", "bcd"};
-	int n_compares[] = {5, 1, 4, 2, 2, 12, 6, 0};
-	int errors = 0;
-	printf("%sINFO: testing strlen.%s\n", GREEN, CLEAR);
-	for (int i = 0; i < 6; i++)
+	printf("%sINFO: testing strlcpy.%s\n", GREEN, CLEAR);
+	char strlcpy_tests[8][2][21] = {{"abc","bca"}, {"\n","\r"}, {"hello","hella"}, {"",""}, {"hello this is a test","hello thi\n is a test"}, {"tests","tests"}, {"there is a test here",""}, {"\x82\x81\xa3","\x02\x01\x23"}};
+	int strlcpy_tests_num[8] = {4, 2, 5, 2, 12, 6, 0, 4};
+	errors = 0;
+	for (int i = 0; i < 8; i++)
 	{
-		data_t *temp = malloc(sizeof(data_t));
-		temp->tests = &i;
-		errors += test(test_strlen, temp);
+		data_t *temp;
+		if(!(temp = malloc(sizeof(data_t)))){
+			printf("%s%sERROR: Failed to allocate memory to test strlcpy.%s\n", BLACK, RED_BG, CLEAR);
+			errors++;
+			continue;
+		}
+		t_list *temp1;
+		if(!(temp1 = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strlcpy.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strlcpy.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->next->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strlcpy.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1->content);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->next->next->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strlcpy.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1->next->content);
+			free(temp1->content);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		temp1->content = strlcpy_tests[i][1];
+		temp1->next->content = strlcpy_tests[i][2];
+		temp1->next->next->content = &(strlcpy_tests_num[i]);
+		temp->tests = temp1;
+		errors += test(test_strlcpy, temp);
+		free(temp1->next->next->content);
+		free(temp1->next->content);
+		free(temp1->content);
+		free(temp1);
+		free(temp);
+	}
+	/*strlcat*/
+	printf("%sINFO: testing strlcat.%s\n", GREEN, CLEAR);
+	char strlcat_tests[8][2][21] = {{"abc","bca"}, {"\n","\r"}, {"hello","hella"}, {"",""}, {"hello this is a test","hello thi\n is a test"}, {"tests","tests"}, {"there is a test here",""}, {"\x82\x81\xa3","\x02\x01\x23"}};
+	int strlcat_tests_num[8] = {4, 2, 5, 2, 12, 6, 0, 4};
+	errors = 0;
+	for (int i = 0; i < 8; i++)
+	{
+		data_t *temp;
+		if(!(temp = malloc(sizeof(data_t)))){
+			printf("%s%sERROR: Failed to allocate memory to test strlcat.%s\n", BLACK, RED_BG, CLEAR);
+			errors++;
+			continue;
+		}
+		t_list *temp1;
+		if(!(temp1 = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strlcat.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strlcat.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->next->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strlcat.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1->content);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->next->next->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strlcat.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1->next->content);
+			free(temp1->content);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		temp1->content = strlcat_tests[i][1];
+		temp1->next->content = strlcat_tests[i][2];
+		temp1->next->next->content = &(strlcat_tests_num[i]);
+		temp->tests = temp1;
+		errors += test(test_strlcat, temp);
+		free(temp1->next->next->content);
+		free(temp1->next->content);
+		free(temp1->content);
+		free(temp1);
+		free(temp);
+	}
+	/*strchr*/
+	printf("%sINFO: testing strchr.%s\n", GREEN, CLEAR);
+	char strchr_tests[8][21] = {"a", "\r", "hella", "", "hello thi\n is a", "tests", "bcd", "\x82\x81\xa3"};
+	char strchr_tests_chr[8] = {"a", "\r", "4", "a", "", "t", "z", "\x81"};
+	errors = 0;
+	for (int i = 0; i < 8; i++)
+	{
+		data_t *temp;
+		if(!(temp = malloc(sizeof(data_t)))){
+			printf("%s%sERROR: Failed to allocate memory to test strchr.%s\n", BLACK, RED_BG, CLEAR);
+			errors++;
+			continue;
+		}
+		t_list *temp1;
+		if(!(temp1 = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strchr.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strchr.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->next->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strchr.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1->content);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		temp1->content = strchr_tests[i];
+		temp1->next->content = strchr_tests_chr[i];
+		temp->tests = temp1;
+		errors += test(test_strchr, temp);
+		free(temp1->next->content);
+		free(temp1->content);
+		free(temp1);
+		free(temp);
+	}
+	if(!errors)
+		printf("%sOK: All tests passed for strlen.%s\n", GREEN, CLEAR);
+	else
+		printf("%sINFO: a total of %d errors were detected in strlen.%s\n", GREEN, errors, CLEAR);
+	/*strrchr*/
+	printf("%sINFO: testing strrchr.%s\n", GREEN, CLEAR);
+	char strrchr_tests[8][21] = {"a", "\r", "hella", "", "hello thi\n is a", "tests", "bcd", "\x82\x81\xa3"};
+	char strrchr_tests_chr[8] = {"a", "\r", "4", "a", "", "t", "z", "\x81"};
+	errors = 0;
+	for (int i = 0; i < 8; i++)
+	{
+		data_t *temp;
+		if(!(temp = malloc(sizeof(data_t)))){
+			printf("%s%sERROR: Failed to allocate memory to test strrchr.%s\n", BLACK, RED_BG, CLEAR);
+			errors++;
+			continue;
+		}
+		t_list *temp1;
+		if(!(temp1 = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strrchr.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strrchr.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->next->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strrchr.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1->content);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		temp1->content = strrchr_tests[i];
+		temp1->next->content = strrchr_tests_chr[i];
+		temp->tests = temp1;
+		errors += test(test_strrchr, temp);
+		free(temp1->next->content);
+		free(temp1->content);
+		free(temp1);
+		free(temp);
+	/*strnstr*/
+	printf("%sINFO: testing strnstr.%s\n", GREEN, CLEAR);
+	char strnstr_tests[8][2][21] = {{"testing program","m"}, {"\ravs","\r"}, {"hello","ll"}, {"",""}, {"hello this is a test","thi\n"}, {"tests","t"}, {"abc","d"}, {"\x82\x81\xa3","\x02\x01\x23"}, {"\x82\x81\xa3","\x82"}};
+	int strnstr_tests_num[8] = {15, 1, 4, 2, 12, 6, 0, 3};
+	errors = 0;
+	for (int i = 0; i < 8; i++)
+	{
+		data_t *temp;
+		if(!(temp = malloc(sizeof(data_t)))){
+			printf("%s%sERROR: Failed to allocate memory to test strnstr.%s\n", BLACK, RED_BG, CLEAR);
+			errors++;
+			continue;
+		}
+		t_list *temp1;
+		if(!(temp1 = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strnstr.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strnstr.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->next->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strnstr.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1->content);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->next->next->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strnstr.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1->next->content);
+			free(temp1->content);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		temp1->content = strnstr_tests[i][1];
+		temp1->next->content = strnstr_tests[i][2];
+		temp1->next->next->content = &(strnstr_tests_num[i]);
+		temp->tests = temp1;
+		errors += test(test_strnstr, temp);
+		free(temp1->next->next->content);
+		free(temp1->next->content);
+		free(temp1->content);
+		free(temp1);
+		free(temp);
+	}
+	/*strncmp*/
+	printf("%sINFO: testing strncmp.%s\n", GREEN, CLEAR);
+	char strncmp_tests[8][2][21] = {{"a","b"}, {"\r","\r"}, {"hello","hella"}, {"",""}, {"hello this is a test","hello thi\n is a"}, {"tests","tests"}, {"abc","bcd"}, {"\x82\x81\xa3","\x02\x01\x23"}};
+	int strncmp_tests_num[8] = {5, 1, 4, 2, 12, 6, 0, 3};
+	errors = 0;
+	for (int i = 0; i < 8; i++)
+	{
+		data_t *temp;
+		if(!(temp = malloc(sizeof(data_t)))){
+			printf("%s%sERROR: Failed to allocate memory to test strncmp.%s\n", BLACK, RED_BG, CLEAR);
+			errors++;
+			continue;
+		}
+		t_list *temp1;
+		if(!(temp1 = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strncmp.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strncmp.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->next->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strncmp.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1->content);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->next->next->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strncmp.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1->next->content);
+			free(temp1->content);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		temp1->content = strncmp_tests[i][1];
+		temp1->next->content = strncmp_tests[i][2];
+		temp1->next->next->content = &(strncmp_tests_num[i]);
+		temp->tests = temp1;
+		errors += test(test_strncmp, temp);
+		free(temp1->next->next->content);
+		free(temp1->next->content);
+		free(temp1->content);
+		free(temp1);
 		free(temp);
 	}
 	if(!errors)
@@ -92,6 +387,58 @@ void test_str()
 	else
 		printf("%sINFO: a total of %d errors were detected in strlen.%s\n", GREEN, errors, CLEAR);
 	/*strjoin*/
+	printf("%sINFO: testing strjoin.%s\n", GREEN, CLEAR);
+	char strjoin_tests[8][2][21] = {{"abc","bca","abcbca"}, {"\n","\r","\n\r"}, {"hello","hella","hellohella"}, {"","",""}, {"hello this is a test","hello thi\n is a test","hello this is a testhello thi\n is a test"}, {"tests","tests","tests"}, {"there is a test here","","there is a test here"}, {"\x82\x81\xa3","\x02\x01\x23","\x82\x81\xa3\x02\x01\x23"}};
+	errors = 0;
+	for (int i = 0; i < 8; i++)
+	{
+		data_t *temp;
+		if(!(temp = malloc(sizeof(data_t)))){
+			printf("%s%sERROR: Failed to allocate memory to test strjoin.%s\n", BLACK, RED_BG, CLEAR);
+			errors++;
+			continue;
+		}
+		t_list *temp1;
+		if(!(temp1 = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strjoin.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strjoin.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->next->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strjoin.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1->content);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		if(!(temp1->next->next->content = malloc(sizeof(t_list)))){
+			printf("%s%sERROR: Failed to allocate memory to test strjoin.%s\n", BLACK, RED_BG, CLEAR);
+			free(temp1->next->content);
+			free(temp1->content);
+			free(temp1);
+			free(temp);
+			errors++;
+			continue;
+		}
+		temp1->content = strjoin_tests[i][1];
+		temp1->next->content = strjoin_tests[i][2];
+		temp1->next->next->content = strjoin_tests[i][3];
+		temp->tests = temp1;
+		errors += test(test_strjoin, temp);
+		free(temp1->next->next->content);
+		free(temp1->next->content);
+		free(temp1->content);
+		free(temp1);
+		free(temp);
 	/*strtrim*/
 }
 
--- libft_tester/str_tester.c
+++ libft_tester/str_tester.c
@@ -5,24 +5,120 @@
 #include "aux.h"
 #include "libft.h"
 
+
 test_t *test_strlen(data_t *p){
 	char *c = (char*)p->tests;
 	char *n, *a, *t;
+	int z,x;
 	test_t *ret;
 	if (!(ret = malloc(sizeof(test_t))))
 		return NULL;
-	if (ft_strlen(*c) != strlen(*c)){
+	z = ft_strlen(*c);
+	x = strlen(*c)
+	if (z != x){
 		ret->code = 1;
-		n = m_strcat("Error detecting type of: \"", c"\"");
-		a = m_strcat(n, ".\n\tFunction returned: ");
-		free(n);
-		t = m_itoa(ft_strlen(*c));
-		n = m_strcat(a, t);
+		t = m_itoa(z);
+		n = m_strcat("Error calculating length.\n\tFunction returned:\n", t);
 		free(t);
-		free(a);
-		a = m_strcat(n, ".\n\tExpected: ");
+		a = m_strcat(n, ".\n\tExpected:\n");
 		free(n);
-		t = m_itoa(strlen(*c));
+		t = m_itoa(x);
+		n = m_strcat(a, t);
+		free(a);
+		free(t);
+		ret->error = m_strcat(n, ".");
+		free(n);
+	}
+	else {
+		ret->code = 0;
+	}
+	return ret;
+}
+
+test_t *test_strlcpy(data_t *p){
+	t_list *c1 = (t_list*)(p->tests);
+	char *n, *a, *t, *str;
+	int z,x;
+	test_t *ret;
+	str = strdup(str, c1->content);
+	if (!(ret = malloc(sizeof(test_t)))){
+		free(str);
+		return NULL;
+	}
+	z = ft_strlcat(c1->content, c1->next->content, (int)c1->next->next->content);
+	x = strlcat(str, c1->next->content, (int)c1->next->next->content);
+	if (z != x || strcmp(str, c1->content) != 0){
+		ret->code = 1;
+		t = m_itoa(z);
+		n = m_strcat("Error copying strings:.\n\tFunction returned:", t);
+		free(t);
+		a = m_strcat(n, "\n\tExpected:\n");
+		free(n);
+		t = m_itoa(x);
+		n = m_strcat(a, t);
+		free(a);
+		free(t);
+		ret->error = m_strcat(n, ".");
+		free(n);
+	} else {
+		ret->code = 0;
+	}
+	free(str);
+	return ret;
+}
+
+test_t *test_strlcat(data_t *p){
+	t_list *c1 = (t_list*)(p->tests);
+	char *n, *a, *t, *str;
+	int z,x;
+	test_t *ret;
+	str = strdup(str, c1->content);
+	if (!(ret = malloc(sizeof(test_t)))){
+		free(str);
+		return NULL;
+	}
+	z = ft_strlcat(c1->content, c1->next->content, (int)c1->next->next->content);
+	x = strlcat(str, c1->next->content, (int)c1->next->next->content);
+	if (z != x || strcmp(str, c1->content) != 0){
+		ret->code = 1;
+		t = m_itoa(z);
+		n = m_strcat("Error concatenating strings:.\n\tFunction returned:", t);
+		free(t);
+		a = m_strcat(n, "\n\tExpected:\n");
+		free(n);
+		t = m_itoa(x);
+		n = m_strcat(a, t);
+		free(a);
+		free(t);
+		ret->error = m_strcat(n, ".");
+		free(n);
+	} else {
+		ret->code = 0;
+	}
+	free(str);
+	return ret;
+}
+
+test_t *test_strchr(data_t *p){
+	t_list *c1 = (t_list*)(p->tests);
+	char *n, *a, *t, *s1, *s2;
+	int size;
+	test_t *ret;
+	if (!(ret = malloc(sizeof(test_t)))){
+		return NULL;
+	}
+	s1 = ft_strchr(c1->content, c1->next->content);
+	s2 = strchr(c1->content, c1->next->content);
+	if (strcmp(s1, s2) != 0){
+		ret->code = 1;
+		size = strlen(s1);
+		t = hex_conv(s1, size);
+		n = m_strcat("Error finding char in string.\n\tFunction returned:\n", t);
+		free(t);
+		a = m_strcat(n, "\n\tExpected: \n");
+		free(n);
+		size = strlen(s2);
+		t = hex_conv(s2, size);
 		n = m_strcat(a, t);
 		free(t);
 		free(a);
@@ -31,47 +127,93 @@ test_t *test_strlen(data_t *p){
 	} else {
 		ret->code = 0;
 	}
+	free(s2);
+	free(s1);
 	return ret;
 }
 
-test_t *test_strlcpy(data_t *p){
-
-}
-
-test_t *test_strlcat(data_t *p){
-
-}
-
-test_t *test_strchr(data_t *p){
-
-}
-
 test_t *test_strrchr(data_t *p){
-
-}
-
-test_t *test_strnstr(data_t *p){
-
-}
-
-test_t *test_strncmp(data_t *p){
-	char *c = (char*)p->tests;
-	char *n, *a, *t;
+	t_list *c1 = (t_list*)(p->tests);
+	char *n, *a, *t, *s1, *s2;
+	int size;
 	test_t *ret;
-	if (!(ret = malloc(sizeof(test_t))))
+	if (!(ret = malloc(sizeof(test_t)))){
 		return NULL;
-	if (ft_strncmp(*c) != strncmp(*c)){
+	}
+	s1 = ft_strrchr(c1->content, c1->next->content);
+	s2 = strrchr(c1->content, c1->next->content);
+	if (strcmp(s1, s2) != 0){
 		ret->code = 1;
-		n = m_strcat("Error detecting type of: \"", c"\"");
-		a = m_strcat(n, ".\n\tFunction returned: ");
+		size = strlen(s1);
+		t = hex_conv(s1, size);
+		n = m_strcat("Error finding char in string.\n\tFunction returned:\n", t);
+		free(t);
+		a = m_strcat(n, "\n\tExpected: \n");
 		free(n);
-		t = m_itoa(ft_strlen(*c));
+		size = strlen(s2);
+		t = hex_conv(s2, size);
 		n = m_strcat(a, t);
 		free(t);
 		free(a);
-		a = m_strcat(n, ".\n\tExpected: ");
+		ret->error = m_strcat(n, ".");
 		free(n);
-		t = m_itoa(strlen(*c));
+	} else {
+		ret->code = 0;
+	}
+	free(s2);
+	free(s1);
+	return ret;
+}
+
+test_t *test_strnstr(data_t *p){
+	t_list *c = (t_list*)(p->tests);
+	char *n, *a, *t, *s1, *s2;
+	int size;
+	test_t *ret;
+	if (!(ret = malloc(sizeof(test_t))))
+		return NULL;
+	s1 = ft_strnstr(c->content, c->next->content, (int)c->next->next->content);
+	s2 = ft_strnstr(c->content, c->next->content, (int)c->next->next->content);
+	if (strcmp(s1, s2) != 0){
+		ret->code = 1;
+		size = strlen(s1);
+		t = hex_conv(s1, size);
+		n = m_strcat("Error joining strings.\n\tFunction returned:\n", t);
+		free(t);
+		a = m_strcat(n, "\n\tExpected: \n");
+		free(n);
+		size = strlen(s2);
+		t = hex_conv(s2, size);
+		n = m_strcat(a, t);
+		free(t);
+		free(a);
+		ret->error = m_strcat(n, ".");
+		free(n);
+	} else {
+		ret->code = 0;
+	}
+	free(s2);
+	free(s1);
+	return ret;
+}
+
+test_t *test_strncmp(data_t *p){
+	t_list *c = (t_list*)(p->tests);
+	char *n, *a, *t;
+	int z,x;
+	test_t *ret;
+	if (!(ret = malloc(sizeof(test_t))))
+		return NULL;
+	z = ft_strncmp(c->content, c->next->content, (int)c->next->next->content);
+	x = strncmp(c->content, c->next->content, (int)c->next->next->content);
+	if (x != z){
+		ret->code = 1;
+		t = m_itoa(z);
+		n = m_strcat("Error comparing strings.\n\tFunction returned:\n", t);
+		free(t);
+		a = m_strcat(n, "\n\tExpected: \n");
+		free(n);
+		t = m_itoa(x);
 		n = m_strcat(a, t);
 		free(t);
 		free(a);
@@ -84,7 +226,33 @@ test_t *test_strncmp(data_t *p){
 }
 
 test_t *test_strjoin(data_t *p){
-
+	t_list *c1 = (t_list*)(p->tests);
+	char *n, *a, *t, *str;
+	int size;
+	test_t *ret;
+	if (!(ret = malloc(sizeof(test_t))))
+		return NULL;
+	str = ft_strjoin(c1->content, c1->next->content);
+	if ((strcmp(str, c1->next->next->content)) != 0){
+		ret->code = 1;
+		size = strlen(str);
+		t = hex_conv(str, size);
+		n = m_strcat("Error joining strings.\n\tFunction returned:\n", t);
+		free(t);
+		a = m_strcat(n, "\n\tExpected: \n");
+		free(n);
+		size = strlen(c1->next->next->content);
+		t = hex_conv(c1->next->next->content, size);
+		n = m_strcat(a, t);
+		free(t);
+		free(a);
+		ret->error = m_strcat(n, ".");
+		free(n);
+	} else {
+		ret->code = 0;
+	}
+	free(str);
+	return ret;
 }
 
 test_t *test_strtrim(data_t *p){
