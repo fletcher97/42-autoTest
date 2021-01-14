f71e4041156b94bbeb0ea58b6cd7736609489b64
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-01-14 10:06:55 +0000

    Quick fix for some bugs, main with params, script executing command with wrong arg, added message for queens problem

--- piscine_tester/C05/mainSrc/main08.c
+++ piscine_tester/C05/mainSrc/main08.c
@@ -2,6 +2,8 @@
 
 int main()
 {
+	printf("\nTesting 08:\n\n\n");
+	printf("If nothing appears after this your program worked fine (according to this tester)\n");
 	ft_ten_queens_puzzle();
 	return 0;
 }
